90237f57de09dcec2114878f0c23bb97a7146171
janamachado <naan.jaan@gmail.com>
2025-05-17 01:50:19 -0300

    chore: Development of middleware for error validation

---
+++ backend/src/middlewares/errorHandler.js
@@ -0,0 +1,18 @@
+function errorHandler(err, req, res, next) {
+  console.error(err.stack);
+
+  if (err.type === 'NotFoundError') {
+    return res.status(404).json({ message: err.message });
+  }
+
+  if (err.type === 'ValidationError') {
+    return res.status(400).json({ message: err.message });
+  }
+
+  return res.status(500).json({ 
+    message: 'Internal server error',
+    error: process.env.NODE_ENV === 'development' ? err.message : undefined
+  });
+}
+
+module.exports = errorHandler;
